42fc3db76af18122a0cd21c3ce9a95e26ea3e115
Votre Nom <vous@exemple.com>
2025-02-01 22:46:04 +0100

    feat: corrected the mse expression

--- nn_from_scratch/nn.py
+++ nn_from_scratch/nn.py
@@ -1,17 +1,23 @@
 import numpy as np
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(- x))
+
+def d_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
 
 lf = {
-    'mse': (lambda y, y_hat : np.linalg.norm(y - y_hat), 
-            lambda y, y_hat : 2 * (y - y_hat)),
+    'mse': (lambda y, y_hat : np.mean(np.linalg.norm(y - y_hat)), 
+            lambda y, y_hat : (2/y.shape[0]) * (y - y_hat)),
     'binary_crossentropy': (lambda y, y_hat : - (y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat)),
-                            lambda y, y_hat : - ((y / np.log(y_hat)) + ((1 - y) / np.log(1 - y_hat)))),
+                            lambda y, y_hat : - ((y / y_hat) + ((1 - y) / (1 - y_hat)))),
 }
 
 af = {
     'linear': (lambda x: x, lambda x: 1),
     'relu': (lambda x: np.max(0, x)),
-    'sigmoid': (lambda x: 1 / (1 + np.exp(- x)), lambda x: 1 / (1 + np.exp(- x)) * (1 - (1 / (1 + np.exp(- x)))))
+    'sigmoid': (sigmoid, d_sigmoid)
 }
 
 
@@ -19,10 +25,10 @@ class ActivationFunction():
     def __init__(self, name):
         self.name = name
     
-    def compute(self):
+    def apply(self):
         return af[self.name][0]
     
-    def compute_derivative(self):
+    def apply_derivative(self):
         return af[self.name][1]
 
 
@@ -30,10 +36,10 @@ class LossFunction():
     def __init__(self, name):
         self.name = name
     
-    def compute(self):
+    def apply(self):
         return lf[self.name][0]
     
-    def compute_derivative(self):
+    def apply_derivative(self):
         return lf[self.name][1]
     
 
@@ -50,23 +56,32 @@ class Optimizer():
         loss_function = LossFunction(loss_function)
         if optimizer == 'gradient_descent':
             for iter in range(n_iter):
-
+                pass
 
 
 class Layer():
-    def __init__(self, input, units, activation):
-        self.n_x = input.shape[1]
+    def __init__(self, input, units, activation='linear'):
+        self.n_x = input.shape[0]
         self.units = units
         self.weights = np.random((self.units, self.n_x))
         self.biases = np.random((self.units, 1))
-        #self.activation = activation
+        self.activation = activation
     
 
 class FFNeuralNetwork():
-    def __init__(self, layers):
+    def __init__(self, layers, inputs, labels, optimizer):
         self.layers = layers
+        self.inputs = inputs
+        self.labels = labels
+        self.optimizer = optimizer
     
+    def initialize(self):
+        pass
+
     def train(self, epochs):
+        inputs = np.copy(self.inputs)
         for epoch in range(epochs):
-            pass
-        
+            for i in range(len(self.layers)):
+                z = self.layers[i].weights @ self.inputs + self.layers[i].biases
+                a = self.layers[i].activation(z)
+                inputs = a
