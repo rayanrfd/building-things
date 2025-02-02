2bf1424e9f215184112e56e9d8bfb9bcffa26ab5
Votre Nom <vous@exemple.com>
2025-02-02 17:20:11 +0100

    feat : started to implement backpropagation

--- nn_from_scratch/nn.py
+++ nn_from_scratch/nn.py
@@ -16,7 +16,7 @@ lf = {
 
 af = {
     'linear': (lambda x: x, lambda x: 1),
-    'relu': (lambda x: np.max(0, x)),
+    'relu': (lambda x: np.max(0, x), lambda x: 0 if x < 0 else x),
     'sigmoid': (sigmoid, d_sigmoid)
 }
 
@@ -44,44 +44,49 @@ class LossFunction():
     
 
 class Optimizer():
-    def __init__(self, learning_rate, weights, biases, loss_function, n_iter, optimizer='gradient_descent'):
+    def __init__(self, learning_rate, loss_functions, n_iter, optimizer='gradient_descent'):
         self.learning_rate = learning_rate
         self.optimizer = optimizer
-        self.weights = weights
-        self.biases = biases
-        self.loss_function = loss_function
+        self.loss_functions = loss_functions
         self.n_iter = n_iter
     
-    def optimize(self, weights, biases, loss_function, n_iter, optimizer='gradient_descent'):
+    def optimize(self, backprop_dict, loss_functions):
         loss_function = LossFunction(loss_function)
-        if optimizer == 'gradient_descent':
-            for iter in range(n_iter):
-                pass
+        if self.optimizer == 'gradient_descent':
+            for iter in range(self.n_iter):
+                for i in range(len(self.layers)):
+                    pass
 
 
 class Layer():
     def __init__(self, input, units, activation='linear'):
         self.n_x = input.shape[0]
         self.units = units
-        self.weights = np.random((self.units, self.n_x))
-        self.biases = np.random((self.units, 1))
+        self.W = np.random((self.units, self.n_x))
+        self.b = np.random((self.units, 1))
         self.activation = activation
     
 
 class FFNeuralNetwork():
-    def __init__(self, layers, inputs, labels, optimizer):
+    def __init__(self, layers, inputs, labels):
         self.layers = layers
         self.inputs = inputs
         self.labels = labels
-        self.optimizer = optimizer
     
     def initialize(self):
         pass
 
-    def train(self, epochs):
-        inputs = np.copy(self.inputs)
+    def train(self, epochs, optimizer):
+        backprop_dict = {f'deriv_layer_{i}': {} for i in range(len(self.layers))}
+        a_i = np.copy(self.inputs)
         for epoch in range(epochs):
             for i in range(len(self.layers)):
-                z = self.layers[i].weights @ self.inputs + self.layers[i].biases
+                backprop_dict[f'deriv_layer_{i}'][f'a_{i - 1}'] = a_i
+                z = self.layers[i].W @ a_i + self.layers[i].b
                 a = self.layers[i].activation(z)
-                inputs = a
+                a_i = a
+                backprop_dict[f'deriv_layer_{i}'][f'z_{i}'] = a_i
+                backprop_dict[f'deriv_layer_{i}'][f'W_{i}'] = self.layers[i].W
+                backprop_dict[f'deriv_layer_{i}'][f'b_{i}'] = self.layers[i].b
+
+            optimizer.optimize(backprop_dict, functions)
