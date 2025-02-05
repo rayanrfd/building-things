2739f2398f88ea746d9f7113a93ff73ff9920418
Votre Nom <vous@exemple.com>
2025-02-05 12:13:16 +0100

    feat: implementation finished, little errors corrected with ChatGPT

--- nn_from_scratch/nn.py
+++ nn_from_scratch/nn.py
@@ -8,72 +8,60 @@ def d_sigmoid(x):
 
 
 lf = {
-    'mse': (lambda y, y_hat : np.mean(np.linalg.norm(y - y_hat)), 
+    'mse': (lambda y, y_hat : np.mean((y - y_hat) ** 2), 
             lambda y, y_hat : (2/y.shape[0]) * (y - y_hat)),
     'binary_crossentropy': (lambda y, y_hat : - (y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat)),
-                            lambda y, y_hat : - ((y / y_hat) + ((1 - y) / (1 - y_hat)))),
+                            lambda y, y_hat : - ((y / y_hat) - ((1 - y) / (1 - y_hat)))),
 }
 
 af = {
     'linear': (lambda x: x, lambda x: 1),
-    'relu': (lambda x: np.max(0, x), lambda x: 0 if x < 0 else 1),
+    'relu': (lambda x: np.maximum(0, x), lambda x: np.where(x > 0, 1, 0)),
     'sigmoid': (sigmoid, d_sigmoid)
 }
 
 
 class ActivationFunction():
     def __init__(self, name):
-        self.name = name
-    
-    def apply(self):
-        return af[self.name][0]
-    
-    def apply_derivative(self):
-        return af[self.name][1]
+        self.func, self.deriv = af[name]
 
 
 class LossFunction():
     def __init__(self, name):
-        self.name = name
-    
-    def apply(self):
-        return lf[self.name][0]
-    
-    def apply_derivative(self):
-        return lf[self.name][1]
+        self.func, self.deriv = lf[name]
     
 
 class Optimizer():
-    def __init__(self, learning_rate, loss_functions, n_iter, optimizer='gradient_descent'):
+    def __init__(self, learning_rate, loss_function: LossFunction, n_iter, optimizer='gradient_descent'):
         self.learning_rate = learning_rate
         self.optimizer = optimizer
-        self.loss_functions = loss_functions
+        self.loss_function = loss_function
         self.n_iter = n_iter
     
-    def optimize(self, backprop_dict, loss_functions, activation_functions):
-        loss_function = LossFunction(loss_function)
+    def optimize(self, backprop_list, layers, activation_functions, labels):
         if self.optimizer == 'gradient_descent':
             for iter in range(self.n_iter):
-                i = len(self.layers)
-                a = backprop_dict[f'deriv_layer_{i}'][f'a_{i}']
-                z = backprop_dict[f'deriv_layer_{i}'][f'z_{i}']
-                delta = loss_function.apply_derivative(a_L, y) * activation_function.apply_derivative(z)
-                for i in range(len(self.layers)):
-                    W -= self.learning_rate * (delta @ a_prev)
-                    b -= self.learning_rate * delta
-                    a = backprop_dict[f'deriv_layer_{i}'][f'a_{i-1}']
-                    z = backprop_dict[f'deriv_layer_{i}'][f'z_{i-1}']
-                    delta = self.layers[i].W.T @ delta * activation_function.apply_derivative(z)
-                    self.layers[i].W = W
-                    self.layers[i].b = b
+                i = len(layers)
+                a = backprop_list[i]['a']
+                z = backprop_list[i]['z']
+                a_prev = backprop_list[i]['a_prev']
+                delta = self.loss_function.deriv(a, labels) * activation_functions[i].deriv(z)
+                for i in range(len(layers) - 1, -1, -1):
+                    d_W = self.learning_rate * np.dot(delta, a_prev)
+                    d_b = self.learning_rate * delta
+                    a = backprop_list[i-1]['a']
+                    z = backprop_list[i-1]['a']
+                    delta = layers[i].W.T @ delta * activation_functions[i].deriv(z)
+                    layers[i].W -= d_W
+                    layers[i].b -= d_b
 
 
 class Layer():
-    def __init__(self, input, units, activation='linear'):
-        self.n_x = input.shape[0]
+    def __init__(self, input_size, units, activation: ActivationFunction):
+        self.n_x = input_size.shape[0]
         self.units = units
-        self.W = np.random((self.units, self.n_x))
-        self.b = np.random((self.units, 1))
+        self.W = np.random.randn(self.units, self.n_x)
+        self.b = np.zeros(self.units, 1)
         self.activation = activation
     
 
@@ -82,24 +70,28 @@ class FFNeuralNetwork():
         self.layers = layers
         self.inputs = inputs
         self.labels = labels
-    
-    def initialize(self):
-        pass
 
     def train(self, epochs, optimizer):
-        # Create a dictionnary to store each values needed for the backpropagation
-        backprop_dict = {f'deriv_layer_{i}': {} for i in range(len(self.layers))}
+        activation_functions = [self.layers[i].activation for i in range(len(self.layers))]
         a_i = np.copy(self.inputs)
         for epoch in range(epochs):
+            # Create a list to store each values needed for the backpropagation
+            backprop_list = []
             for i in range(len(self.layers)):
                 # Perform the forward propagation through each layer
-                # and stores each value computed
-                backprop_dict[f'deriv_layer_{i}'][f'a_{i - 1}'] = a_i
+                # and stores each value computed in a dictionnary
+                a_prev = a_i
                 z = self.layers[i].W @ a_i + self.layers[i].b
-                a = self.layers[i].activation(z)
+                a = activation_functions[i].func(z)
                 a_i = a
-                backprop_dict[f'deriv_layer_{i}'][f'z_{i}'] = a_i
-                backprop_dict[f'deriv_layer_{i}'][f'W_{i}'] = self.layers[i].W
-                #backprop_dict[f'deriv_layer_{i}'][f'b_{i}'] = self.layers[i].b
+                backprop_list.append({'a_prev': a_prev, 'z': z, 'a': a, 'layer': i})
 
-            optimizer.optimize(backprop_dict, functions)
+            optimizer.optimize(backprop_list, self.layers, activation_functions, self.labels)
+    
+    def inference(self, inputs):
+        a_i = np.copy(inputs)
+        for i in range(len(self.layers)):
+            z = self.layers[i].W @ a_i + self.layers[i].b
+            a = self.layers[i].activation.func(z)
+            a_i = a
+        return a_i
